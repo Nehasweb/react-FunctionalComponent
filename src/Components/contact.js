d1c4b4ac8d361503e428fe91d37980e87ce72bb7
Neha Gholap <nehagholap98@gmail.com>
2024-03-12 19:57:14 +0530

    First React

---
+++ src/Components/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Contact()
+{
+    return (<><h1>Contact...!(Functional Component) </h1>
+    <input type="text"/>
+    <button>Click</button>
+    </>)
+    
+}
+
+
+export default Contact;
